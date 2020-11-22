29edaf2bceb8bb4aaa9b34ebb3bf88580710220e
grzes <majmurek@wszib.edu.pl>
2020-11-22 15:06:07 +0100

    init youtube api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ContentComponent } from '../content/content.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
 
   ],
   providers: [],
---
+++ src/content/content.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  defaultID: any = 'BCy3Y_pE-l0';
+
+  constructor(public httpClient: HttpClient, private dom: DomSanitizer) { }
+   get url(): SafeUrl {
+     const a  = `https://www.youtube.com/embed/${this.id}`;
+     return this.dom.bypassSecurityTrustResourceUrl(a);
+   }
+
+   get id():string {
+    return this.defaultID;
+   }
+
+
+  ngOnInit(): void {
+  let params = new HttpParams();
+  const data = {
+     part: 'snippet',
+     key: 'AIzaSyAVPRtNBbTqBey08rHQRrZPQHgDWYCFcr4',
+     maxResults: 9,
+     playlistId: 'PLlCWUQK37jVjFbbdZ0hDQQ1iaX_y5Y-sf'
+   };
+  Object.keys(data).forEach(p => {
+    params = params.append(p, data[p]);
+});
+  this.httpClient.get('https://www.googleapis.com/youtube/v3/playlistItems', {params})
+    .subscribe((res: any) => {
+      console.log(res); 
+      this.defaultID = res.items[0].snippet.resourceId.videoId});
+  }
+
+}
